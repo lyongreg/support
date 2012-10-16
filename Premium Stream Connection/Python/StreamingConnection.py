0550618c9e7a86838786b448a808be4cb83fc814
compston <stephen@gnipicentral.com>
2012-10-16 15:24:13 -0600

    sc: updated streaming python snippet for better disconnect handling and automatic reconnects

--- Premium Stream Connection/Python/StreamingConnection.py
+++ Premium Stream Connection/Python/StreamingConnection.py
@@ -5,64 +5,65 @@ import base64
 import zlib
 import threading
 from threading import Lock
-from cStringIO import StringIO
 import json
 import sys
+import ssl
 
-# Tune these as needed. 
-# Note that a MAXBUFFSIZE of 20*CHUNKSIZE corresponds to around 10 Tweets.
-# So, if you are consuming a low volume stream, the Activities will sit in the buffer until ~10 are sent into the buffer.
+# Tune CHUNKSIZE as needed. 
+# For high volume streams, use large chuck sizes, for low volume streams, decrease
+# CHUNKSIZE.  Minimum practical is about 1K.
 
-CHUNKSIZE = 1024
-MAXBUFFSIZE = 20*CHUNKSIZE
-
+CHUNKSIZE = 4*1024
+GNIPKEEPALIVE = 30  # seconds
 NEWLINE = '\r\n'
 
-url = "YOUR_STREAM_URL_HERE"
-UN = 'YOUR_USERNAME_HERE'
-PWD = 'YOUR_PASSWORD_HERE'
+URL = 'URL'
+UN = 'UN'
+PWD = 'PWD'
+HEADERS = { 'Accept': 'application/json',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding' : 'gzip',
+            'Authorization' : 'Basic %s' % base64.encodestring('%s:%s' % (UN, PWD))  }
 
 print_lock = Lock()
+err_lock = Lock()
 
 class procEntry(threading.Thread):
-	def __init__(self, buf):
-		self.bufList = [x.strip() for x in buf.split(NEWLINE) if x.strip() <> '']
-		threading.Thread.__init__(self)
+    def __init__(self, buf):
+        self.buf = buf
+        threading.Thread.__init__(self)
 
-	def run(self):
-		try:
-			self.output(self.bufList)
-		except Exception, e:  # v hard to debug, catches everything! Replace to make more helpful.
-			sys.stderr.write("thread failed: (%s)\n"%e)
+    for rec in [x.strip() for x in self.buf.split(NEWLINE) if x.strip() <> '']:
+            try:
+                jrec = json.loads(rec.strip())
+                tmp = json.dumps(jrec)
+                with print_lock:
+                    print(tmp)
+            except ValueError, e:
+                with err_lock:
+                    sys.stderr.write("Error processing JSON: %s (%s)\n"%(str(e), rec))
 
-	def output(self, bufList):
-		for rec in bufList:
-			jrec = json.loads(rec.strip())
-			with print_lock:
-				print(json.dumps(jrec))
-
-def get():
-    headers = {
-    'Accept': 'application/json',
-    'Connection': 'keep-alive',
-    'Accept-Encoding' : 'gzip',
-    'Authorization' : 'Basic %s' % base64.encodestring('%s:%s' % (UN, PWD))
-    }
-    req = urllib2.Request(url, headers=headers)
-    response = urllib2.urlopen(req)
-    
-    d = zlib.decompressobj(16+zlib.MAX_WBITS)
-
-    ldata = StringIO()    	
-    bufSize = 0
+def getStream():
+   req = urllib2.Request(URL, headers=HEADERS)
+    response = urllib2.urlopen(req, timeout=(1+GNIPKEEPALIVE))
+    # header -  print response.info()
+    decompressor = zlib.decompressobj(16+zlib.MAX_WBITS)
+    remainder = ''
     while True:
-	tmpString = d.decompress(response.read(CHUNKSIZE))
-	bufSize += len(tmpString)
-    	ldata.write(tmpString)
-	if tmpString.endswith(NEWLINE) and bufSize > MAXBUFFSIZE:
-		procEntry(ldata.getvalue()).start()
-		ldata = StringIO()
-		bufSize = 0
+        tmp = decompressor.decompress(response.read(CHUNKSIZE))
+        if tmp == '':
+            return
+        [records, remainder] = ''.join([remainder, tmp]).rsplit(NEWLINE,1)
+        procEntry(records).start()
 
 if __name__ == "__main__":
-    get()
+  
+    # Note that this performs automatic reconnects to the stream, in an infinite loop
+    while True:
+        try:
+            getStream()
+            with err_lock:
+                sys.stderr.write("Forced disconnect: %s\n"%(str(e)))
+        except ssl.SSLError, e:
+            with err_lock:
+                sys.stderr.write("Connection failed: %s\n"%(str(e)))
