b54fa91afc06d9a0bc3b5a6898d60d161f30c7cf
Jim Moffitt <jmoffitt@gnipcentral.com>
2013-10-01 11:21:03 -0600

    Update DeleteRule.py
    Added error handler around urllib2.urlopen(req).

--- Rules API/Python/DeleteRule.py
+++ Rules API/Python/DeleteRule.py
@@ -55,5 +55,10 @@ if __name__ == "__main__":
 
 	req.add_header("Authorization", "Basic %s" % base64string)
 
-	response = urllib2.urlopen(req)
+	try:
+		response = urllib2.urlopen(req)
+	except urllib2.HTTPError as e:
+        	print e.read()
+
+	
 	the_page = response.read()
